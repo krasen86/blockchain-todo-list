10b923ad61df6c2e9a4f7ea6a31136b28ead9fbb
Krasen Parvanov <krasenparvanov@gmail.com>
2022-02-14 22:25:20 +0100

    Add create task function with dummy constructor

--- contracts/TodoList.sol
+++ contracts/TodoList.sol
@@ -3,4 +3,21 @@ pragma solidity 0.8.11;
 contract TodoList {
 
     uint public taskCount = 0; // state variable written directly to the blockchain
+
+    struct Task {
+        uint taskID;
+        string content;
+        bool completed;
+    }
+
+    mapping(uint => Task) public tasks;
+
+    constructor() public {
+        createTask("Work on Thesis");
+    }
+
+    function createTask(string memory _content) public {
+        taskCount++;
+        tasks[taskCount] = Task(taskCount, _content, false);
+    }
 }
